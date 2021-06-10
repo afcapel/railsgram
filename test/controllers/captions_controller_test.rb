0e061a358b04abb4df82ac03e75940adbc8064c8
Alberto F. Capel <afcapel@gmail.com>
2021-06-10 13:45:43 +0100

    Add some controller tests

---
+++ test/controllers/captions_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class CaptionsControllerTest < ActionDispatch::IntegrationTest
+
+  setup do
+    @post = posts(:red_fuji)
+  end
+
+  test "GET :edit" do
+    get edit_post_caption_path(@post)
+    assert_response :success
+  end
+
+  test "PATCH :update" do
+    patch post_caption_path(@post), params: {
+      post: { caption: "Updated caption!" }
+    }
+
+    assert_redirected_to root_path
+
+    assert_equal  "Updated caption!", @post.reload.caption
+  end
+end
---
+++ test/controllers/pictures_controller_test.rb
@@ -0,0 +1,29 @@
+require "test_helper"
+
+class PicturesControllerTest < ActionDispatch::IntegrationTest
+
+  setup do
+    @post = posts(:red_fuji)
+  end
+
+  test "GET :edit" do
+    get edit_post_picture_path(@post)
+    assert_response :success
+  end
+
+  test "PATCH :update" do
+    original_blob = @post.picture.blob
+
+    picture_data = file_fixture("great_wave.jpg").read
+    encoded = Base64.encode64(picture_data)
+
+    patch post_picture_path(@post, params: {
+      post: {
+        picture_data: "data:image/jpg;base64,#{encoded}"
+      }
+    })
+    assert_redirected_to edit_post_caption_path(@post)
+
+    assert_not_equal original_blob.key, @post.reload.picture.blob.key
+  end
+end
